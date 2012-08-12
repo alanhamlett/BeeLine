4ce3b6411e36fa6c735798857e53312b6774f263
Alan Hamlett <alan.hamlett@gmail.com>
2012-08-12 01:41:49 -0700

    box.com/net api login working

---
+++ src/background.js
@@ -0,0 +1,17 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    var currentUrl = tab.url;
+    console.log(currentUrl);
+    if (currentUrl !== undefined) {
+        var params = currentUrl.split('&');
+        for (var a = 0; a < params.length; a++) {
+            var param = params[a].split('=');
+            if (param[0] === 'auth_token') {
+                var index = chrome.extension.getURL('index.html');
+                var new_url = index + '?auth_token=' + param[1];
+                chrome.storage.local.set({'box-auth-token': param[1]}, function() {
+                    chrome.tabs.update(tabId, {url: new_url});
+                });
+            }
+        }
+    }
+});
